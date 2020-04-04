b3a482b1f8fdd63179d9b52738764e94b196d354
Timur Mishiev <timurmishiev@gmail.com>
2020-04-04 23:49:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -93,7 +93,14 @@ class Dog
     end.first
   end
   
-  
+    def update
+      sql = <<-SQL
+      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
+     end
+
   
   
 end 
